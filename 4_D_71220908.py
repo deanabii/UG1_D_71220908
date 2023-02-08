e25d2ce73d94dc282559576a9adbf4cef6c18835
deanabii <117475244+deanabii@users.noreply.github.com>
2023-02-08 19:16:30 +0700

    Add files via upload

---
+++ 4_D_71220908.py
@@ -0,0 +1,9 @@
+print("-"*8, "Nilai Ke 1", "-"*8)
+harian1 = input("Nilai Harian :")
+uts1 = input("Nilai UTS :")
+uas1 = input("Nilai UAS : ")
+
+print("-"*8, "Nilai Ke 2", "-"*8)
+harian2 = input("Nilai Harian :")
+uts2 = input("Nilai UTS :")
+uas2 = input("Nilai UAS : ")
